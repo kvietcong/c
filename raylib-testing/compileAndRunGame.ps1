200001b6313b5d8b74ec56d12bcbee34d136ef34
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-10-23 10:25:18 -0700

    👟 Update Run Script

--- raylib-testing/compileAndRunGame.ps1
+++ raylib-testing/compileAndRunGame.ps1
@@ -1,10 +1,11 @@
 <# Raylib Compile & Run Script #>
+<# I should probably make a Make file or something XD #>>
 
-gcc <# Compile Game #> `
+gcc <# Compile Game with GCC #> `
     raylib-testing.c <# Entry-Point C File #> `
     -o ./game.exe <# Output File Path #> `
     -O1 -Wall <# Optimizations and Warning Flags #> `
     -L lib/ <# Including Library Path #> `
-    -l raylib -l opengl32 -l gdi32 -l winmm <# Including Actual Libraries #> `
+    -l raylib -l opengl32 -l gdi32 -l winmm <# Including Raylib Libraries #> `
 && `
 ./game.exe <# Run Game #>
