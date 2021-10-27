523bfe72329a1d6ab98b0c933740603c04d6c8ea
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-10-26 20:17:00 -0700

    🌌 Make Physics independent from rendering

--- raylib-testing/raylib-testing.c
+++ raylib-testing/raylib-testing.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "include/raylib.h"
 #include "include/raymath.h"
 
-typedef struct Window {
+typedef struct Window
+{
     unsigned int width;
     unsigned int height;
 } Window;
 
-typedef struct Player {
+typedef struct Player
+{
     Rectangle rect;
     Color color;
     Vector2 velocity;
@@ -19,99 +22,139 @@ typedef struct Player {
     float mass;
 } Player;
 
-typedef struct RectangleEnv {
+typedef struct RectangleEnv
+{
     Rectangle rect;
     Color color;
     unsigned int state;
 } RectangleEnv;
 
+void printVec2(Vector2 rec);
+void printRec(Rectangle rec);
 Vector2 getTarget(Camera2D camera, Player player);
-void updootPlayer(Player* player, const RectangleEnv elements[], int elementsSize, float deltaTime);
 unsigned int checkUnsignedIntBit(unsigned int item, unsigned int n);
-void printRec(RectangleEnv rec);
+void updootPlayer(Player *player,
+                  const RectangleEnv elements[],
+                  int elementsSize,
+                  float deltaTime);
 
 const float GRAVITY = -9.8;
-const int TARGET_FPS = 144;
 const int COLLISION_ALLOWANCE = 5;
+const double PHYSICS_DELTA = 1.0 / 128.0;
 
+bool isChangingFrames = false;
 bool goalReached = false;
 bool resetGame = true;
+int maxFPS = 144;
 
 int main(void)
 {
     // Initializing variables
-    const Window window = { 1280, 720 };
+    const Window window = {1280, 720};
     Color backgroundColor = BLACK;
 
+    if (!isChangingFrames) { SetConfigFlags(FLAG_VSYNC_HINT); }
     InitWindow(window.width, window.height, "Raylib Testing");
-    SetTargetFPS(TARGET_FPS);
 
-    Player player;
-    RectangleEnv elements[255];
-    int elementsSize = sizeof(elements) / sizeof(elements[0]);
-
-    Player defaultPlayer = {
-        .rect = { 15, 15, 50, 50 },
-        .color = RED,
-        .velocity = { 0, 0 },
-        .acceleration = 300, .jumpStrength = 3.5,
-        .maxBoost = 100, .boostCharge = 0, .boostStrength = 2,
-        .mass = 74
-    };
-
-    RectangleEnv defaultElements[] = {
-        { { -10000, window.height * 2, 20000, 100 }, GRAY, 1 },
-        { { 0, window.height / 2, window.width, 100 }, GRAY, 1 },
-        { { 500, 0, 100, window.height }, GRAY, 1 },
-        { { 450, 300, 50, 10 }, GRAY, 1 },
-        { { 200, 250, 50, 10 }, GRAY, 1 },
-        { { 195, 205, 10, 50 }, GRAY, 1 },
-        { { 300, 160, 50, 10 }, GRAY, 1 },
-        { { 100, 100, 100, 10 }, GRAY, 1 },
-        { { 450, 50, 50, 10 }, GRAY, 1 },
-
-        { { 525, -100, 50, 50 }, GREEN, 2 },
-    };
-    const int defaultElementsSize = sizeof(defaultElements) / sizeof(defaultElements[0]);
-
-    Camera2D camera = { 0 };
-    camera.target = getTarget(camera, player);
-    camera.offset = (Vector2) { window.width / 2.0f, window.height / 2.0f };
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
-
-    // Image loading must happen after window initialization
     Image backgroundImage = LoadImage("./resources/space.png");
     Texture2D backgroundTexture = LoadTextureFromImage(backgroundImage);
     UnloadImage(backgroundImage);
 
+    Player defaultPlayer = {
+        .rect = {15, 15, 50, 50},
+        .color = RED,
+        .velocity = {0, 0},
+        .acceleration = 300,
+        .jumpStrength = 3.5,
+        .maxBoost = 100,
+        .boostCharge = 0,
+        .boostStrength = 2,
+        .mass = 74
+    };
+
+    Player player;
+    player = defaultPlayer;
+
+    RectangleEnv defaultElements[] = {
+        {{-10000, window.height * 2, 20000, 100}, GRAY, 1},
+        {{0, window.height / 2, window.width, 100}, GRAY, 1},
+        {{500, 0, 100, window.height}, GRAY, 1},
+        {{450, 300, 50, 10}, GRAY, 1},
+        {{200, 250, 50, 10}, GRAY, 1},
+        {{195, 205, 10, 50}, GRAY, 1},
+        {{300, 160, 50, 10}, GRAY, 1},
+        {{100, 100, 100, 10}, GRAY, 1},
+        {{450, 50, 50, 10}, GRAY, 1},
+
+        {{525, -100, 50, 50}, GREEN, 2},
+    };
+    const int defaultElementsSize =
+        sizeof(defaultElements) / sizeof(defaultElements[0]);
+
+    RectangleEnv elements[255];
+    int elementsSize = sizeof(elements) / sizeof(elements[0]);
+
+    Camera2D camera = {0};
+    camera.target = getTarget(camera, player);
+    camera.offset = (Vector2){window.width / 2.0f, window.height / 2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    double physicsTotalTimeElapsed = 0.0;
+    double frameTotalTitleElapsed = 0.0;
+    double physicsTimeToCatchUp = 0.0;
+
     // Main game loop
     while (!WindowShouldClose())
     {
-        BeginDrawing();
+        if (isChangingFrames) { SetTargetFPS(maxFPS); }
+
+        float frameDeltaTime = GetFrameTime();
+        frameTotalTitleElapsed += frameDeltaTime;
+
+        if (resetGame)
         {
-            if (resetGame) {
-                player = defaultPlayer;
-                for (int i = 0; i < defaultElementsSize; i++)  {
-                    elements[i] = *(defaultElements + i);
-                }
-                for (int i = defaultElementsSize; i < elementsSize; i++) {
-                    elements[i] = (RectangleEnv) { 0 };
-                }
-                resetGame = false;
+            isChangingFrames = false;
+            goalReached = false;
+            maxFPS = 144;
+
+            player = defaultPlayer;
+            for (int i = 0; i < defaultElementsSize; i++)
+            {
+                elements[i] = *(defaultElements + i);
+            }
+            for (int i = defaultElementsSize; i < elementsSize; i++)
+            {
+                elements[i] = (RectangleEnv){0};
             }
 
+            resetGame = false;
+        }
+
+        if (IsKeyPressed(KEY_EQUAL)) { maxFPS += 20; }
+        if (IsKeyPressed(KEY_MINUS)) { maxFPS -= 20; }
+
+        physicsTimeToCatchUp += frameDeltaTime;
+        while (physicsTimeToCatchUp >= PHYSICS_DELTA)
+        {
+            camera.target = getTarget(camera, player);
+            updootPlayer(&player, elements, elementsSize, PHYSICS_DELTA);
+
+            physicsTimeToCatchUp -= PHYSICS_DELTA;
+            physicsTotalTimeElapsed += PHYSICS_DELTA;
+        }
+
+        if (IsKeyPressed(KEY_R)) { resetGame = true; }
+
+        BeginDrawing();
+        {
             ClearBackground(backgroundColor);
             DrawTexture(backgroundTexture, 0, 0, WHITE);
 
-            const float deltaTime = GetFrameTime();
-
-            updootPlayer(&player, elements, elementsSize, deltaTime);
-
-            camera.target = getTarget(camera, player);
             BeginMode2D(camera);
             {
-                for (int i = 0; i < elementsSize; i++) {
+                for (int i = 0; i < elementsSize; i++)
+                {
                     DrawRectangleRec(elements[i].rect, elements[i].color);
                 }
                 DrawRectangleRec(player.rect, player.color);
@@ -121,17 +164,14 @@ int main(void)
             DrawFPS(0, 0);
             char boostChargeText[32];
             sprintf(boostChargeText, "Boost Fuel: %i/%i",
-                (int) player.boostCharge, (int) player.maxBoost);
+                    (int)player.boostCharge, (int)player.maxBoost);
             DrawText(boostChargeText, 25, window.height - 50, 25, BLUE);
-            if (goalReached) {
+
+            if (goalReached)
+            {
                 DrawText("YOU WON!",
                          window.width / 2, window.height / 2,
                          72, GREEN);
-            }
-
-            if (IsKeyPressed(KEY_R)) {
-                resetGame = true;
-                goalReached = false;
             }
         }
         EndDrawing();
@@ -140,32 +180,41 @@ int main(void)
     UnloadTexture(backgroundTexture);
     CloseWindow();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-unsigned int checkUnsignedIntBit(unsigned int item, unsigned int n) {
+unsigned int checkUnsignedIntBit(unsigned int item, unsigned int n)
+{
     return item & (1 << n);
 }
 
-void printRec(Rectangle rec) {
-    printf("(x: %f, y: %f, width: %f, height: %f)\n", rec.x, rec.y, rec.width, rec.height);
+void printVec2(Vector2 vec) {
+    printf("(%f, %f)\n", vec.x, vec.y);
+}
+
+void printRec(Rectangle rec)
+{
+    printf("(x: %f, y: %f, width: %f, height: %f)\n",
+           rec.x, rec.y, rec.width, rec.height);
 }
 
 void updootPlayer(
-    Player* player,
+    Player *player,
     const RectangleEnv elements[], int elementsSize,
-    float deltaTime
-) {
+    float deltaTime)
+{
     bool isOnGround = false;
     int hasHitWall = 0;
 
     int pX = player->rect.x, pY = player->rect.y,
         pH = player->rect.height, pW = player->rect.width;
-    for (int i = 0; i < elementsSize; i++) {
-        if (
-            checkUnsignedIntBit(elements[i].state, 1)
-            && CheckCollisionRecs(player->rect, elements[i].rect)
-        ) goalReached = true;
+    for (int i = 0; i < elementsSize; i++)
+    {
+        if (checkUnsignedIntBit(elements[i].state, 1)
+         && CheckCollisionRecs(player->rect, elements[i].rect))
+        {
+            goalReached = true;
+        }
 
         // Check for collidable flag (first bit)
         if (!checkUnsignedIntBit(elements[i].state, 0)) continue;
@@ -173,15 +222,17 @@ void updootPlayer(
         const int eX = elements[i].rect.x, eY = elements[i].rect.y,
                   eW = elements[i].rect.width, eH = elements[i].rect.height;
         if (pY + pH > eY + COLLISION_ALLOWANCE
-            && pY < eY + eH - COLLISION_ALLOWANCE
-        ) {
-            if (pX + pW > eX + eW && pX < eX + eW) {
+         && pY < eY + eH - COLLISION_ALLOWANCE)
+        {
+            if (pX + pW > eX + eW && pX < eX + eW)
+            {
                 player->rect.x = eX + eW;
                 hasHitWall = 1;
                 pX = eX + eW;
                 player->velocity.x = 0;
             }
-            if (pX < eX && pX + pW > eX) {
+            if (pX < eX && pX + pW > eX)
+            {
                 player->rect.x = eX - pW;
                 hasHitWall = 1;
                 pX = eX - pW;
@@ -189,14 +240,16 @@ void updootPlayer(
             }
         }
         if (pX + pW > eX + COLLISION_ALLOWANCE
-            && pX < eX + eW - COLLISION_ALLOWANCE
-        ) {
-            if (pY + pH > eY + eH && pY < eY + eH) {
+         && pX < eX + eW - COLLISION_ALLOWANCE)
+        {
+            if (pY + pH > eY + eH && pY < eY + eH)
+            {
                 player->rect.y = eY + eH;
                 pY = eY + eH;
                 player->velocity.y = 0;
             }
-            if (pY <= eY && pY + pH >= eY) {
+            if (pY <= eY && pY + pH >= eY)
+            {
                 player->rect.y = eY - pH;
                 pY = eY - pH;
                 isOnGround = true;
@@ -207,31 +260,38 @@ void updootPlayer(
 
     player->velocity.x *= (isOnGround ? 0.50 : 0.40) * deltaTime;
 
-    if (isOnGround) player->boostCharge += 40 * deltaTime;
-    if (player->boostCharge > player->maxBoost) player->boostCharge = player->maxBoost;
-    if (!isOnGround) player->velocity.y -= GRAVITY * deltaTime;
-
-    if (IsKeyDown(KEY_D) && hasHitWall <= 0) player->velocity.x += player->acceleration * deltaTime;
-    if (IsKeyDown(KEY_A) && hasHitWall >= 0) player->velocity.x -= player->acceleration * deltaTime;
-    if (IsKeyDown(KEY_W) && isOnGround) player->velocity.y -= player->jumpStrength;
+    if (isOnGround)
+        player->boostCharge += 40 * deltaTime;
+    if (player->boostCharge > player->maxBoost)
+        player->boostCharge = player->maxBoost;
+    if (!isOnGround)
+        player->velocity.y -= GRAVITY * deltaTime;
+    if (IsKeyDown(KEY_D) && hasHitWall <= 0)
+        player->velocity.x += player->acceleration * deltaTime;
+    if (IsKeyDown(KEY_A) && hasHitWall >= 0)
+        player->velocity.x -= player->acceleration * deltaTime;
+    if (IsKeyDown(KEY_W) && isOnGround)
+        player->velocity.y -= player->jumpStrength;
     if (IsKeyDown(KEY_SPACE)
-        && !isOnGround
-        && (IsKeyDown(KEY_A) || IsKeyDown(KEY_D))
-        && player->boostCharge > 0
-    ) {
+     && !isOnGround
+     && (IsKeyDown(KEY_A) || IsKeyDown(KEY_D))
+     && player->boostCharge > 0)
+    {
         player->boostCharge -= 100 * deltaTime;
-        if (player->boostCharge < 0) player->boostCharge = 0;
+        if (player->boostCharge < 0)
+            player->boostCharge = 0;
         player->velocity.x *= player->boostStrength;
     }
 
-    player->rect.x = (int) player->velocity.x + player->rect.x;
-    player->rect.y = (int) player->velocity.y + player->rect.y;
+    player->rect.x += player->velocity.x;
+    player->rect.y += player->velocity.y;
 }
 
-Vector2 getTarget(Camera2D camera, Player player) {
+Vector2 getTarget(Camera2D camera, Player player)
+{
     Vector2 playerCenter = {
         player.rect.x + player.rect.width / 2,
-        player.rect.y + player.rect.width / 2
+        player.rect.y + player.rect.height / 2
     };
     Vector2 target = Vector2Lerp(camera.target, playerCenter, 1.0 / 20.0);
     return target;
